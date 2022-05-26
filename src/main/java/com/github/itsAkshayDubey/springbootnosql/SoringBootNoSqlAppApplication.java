6be5b9dcf4c5c8a97ff71dc6a69c879b16a872f4
Akshay Dubey <akshay.dubey055@gmail.com>
2022-05-27 03:14:14 +0530

    Initial commit

---
+++ src/main/java/com/github/itsAkshayDubey/springbootnosql/SoringBootNoSqlAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.itsAkshayDubey.springbootnosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoringBootNoSqlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoringBootNoSqlAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/itsAkshayDubey/springbootnosql/SoringBootNoSqlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.itsAkshayDubey.springbootnosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoringBootNoSqlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
