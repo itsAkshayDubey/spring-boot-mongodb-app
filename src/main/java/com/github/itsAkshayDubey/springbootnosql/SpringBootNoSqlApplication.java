1704fb2ab17410a3e600adff509165d7cf995546
Akshay Dubey <akshay.dubey055@gmail.com>
2022-05-27 11:56:05 +0530

    Initial commit

--- src/main/java/com/github/itsAkshayDubey/springbootnosql/SpringBootNoSqlApplication.java
+++ src/main/java/com/github/itsAkshayDubey/springbootnosql/SpringBootNoSqlApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SoringBootNoSqlAppApplication {
+public class SpringBootNoSqlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SoringBootNoSqlAppApplication.class, args);
+		SpringApplication.run(SpringBootNoSqlApplication.class, args);
 	}
 
 }
