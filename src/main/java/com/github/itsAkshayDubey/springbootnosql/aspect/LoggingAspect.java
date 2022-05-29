eb69f183440427c79770d230eac76a4a2f5cb6e0
Akshay Dubey <akshay.dubey055@gmail.com>
2022-05-29 09:40:38 +0530

    feat: Add logging aspect

---
+++ src/main/java/com/github/itsAkshayDubey/springbootnosql/aspect/LoggingAspect.java
@@ -0,0 +1,59 @@
+package com.github.itsAkshayDubey.springbootnosql.aspect;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass()); 
+
+	@Pointcut("within(@org.springframework.stereotype.Repository *)" +
+			" || within(@org.springframework.stereotype.Service *)" +
+			" || within(@org.springframework.web.bind.annotation.RestController *)")
+	public void springBeanPointcut() {
+	}
+
+	@Pointcut("within(com.github.itsAkshayDubey.springbootnosql..*)" +
+			" || within(com.github.itsAkshayDubey.springbootnosql.service..*)" +
+			" || within(com.github.itsAkshayDubey.springbootnosql.controller..*)")
+	public void applicationPackagePointcut() {
+	}
+
+	@AfterThrowing(pointcut = "applicationPackagePointcut() && springBeanPointcut()", throwing = "e")
+	public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+		LOGGER.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
+				joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
+	}
+
+	@Around("applicationPackagePointcut() && springBeanPointcut()")
+	public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
+					joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+		}
+		try {
+			Object result = joinPoint.proceed();
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
+						joinPoint.getSignature().getName(), result);
+			}
+			return result;
+		} catch (IllegalArgumentException e) {
+			LOGGER.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
+					joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+			throw e;
+		}
+	}
+
+}
